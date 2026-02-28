9e9d4d3aef01ec162abddf1e87730ba633d51555
Rom-Stein <rom1livebox@gmail.com>
2026-02-28 05:34:19 +0100

    Item Tracker update
    - Re-organized the layout
    - Prepared themed item grids
    - Introduced a 2nd variant with detailed grids with everything
    - Attempted to make Gems Packs compatible with autotracking
    
    To do :
    - test autotracker
    - introduce all crystal and force gem items in the detailed tracker
    - prepare for the introduction of the map tracker

---
+++ scripts/autotracking/slots_data_mapping.lua
@@ -0,0 +1,3 @@
+SLOTS_DATA_MAPPING = {
+	["spirit_gem_packs"] = {"gem_packs","INT",{1,20}},
+}
--- scripts/items_import.lua
+++ scripts/items_import.lua
@@ -1,6 +1,3 @@
 
 Tracker:AddItems("items/items.json")
-Tracker:AddItems("items/hint_items.json")
-Tracker:AddItems("items/location_items.json")
-Tracker:AddItems("items/labels.json")
-                
+Tracker:AddItems("items/settings.json")
